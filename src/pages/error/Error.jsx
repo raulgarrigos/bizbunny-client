2d3702fc3e87e55151e5f8efd6d0fd0f9f88e69a
raulgp <raul.garrigos@hotmail.com>
2024-02-28 13:48:13 +0100

    styles to errors

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+
 function Error() {
   return (
-    <div>
-      <h3>Error 500. We have some problems with the server 😅</h3>
+    <div className="error-page">
+      <h3 className="error-title">
+        Error 500. We have some problems with the server 😅
+      </h3>
+      <Link to="/">
+        <Button
+          variant="primary"
+          className="custom-button"
+          style={{ margin: "20px" }}
+        >
+          Go to Home
+        </Button>
+      </Link>
     </div>
   );
 }
--- src/pages/error/NotFound.jsx
+++ src/pages/error/NotFound.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+
 function NotFound() {
   return (
-    <div>
-      <h3>Error 404. Page not found</h3>
+    <div className="error-page">
+      <h3 className="error-title">Error 404. Page not found</h3>
+      <Link to="/">
+        <Button
+          variant="primary"
+          className="custom-button"
+          style={{ margin: "20px" }}
+        >
+          Go to Home
+        </Button>
+      </Link>
     </div>
   );
 }
