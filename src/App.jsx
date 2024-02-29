aa8ada48819a28943d3d0df03591f4241496da54
raulgp <raul.garrigos@hotmail.com>
2024-02-29 12:35:15 +0100

    saved theme preference

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Routes, Route } from "react-router";
 import { Button } from "react-bootstrap";
 import { ThemeContext } from "./context/theme.context";
@@ -9,27 +9,38 @@ import Error from "./pages/error/Error";
 import NotFound from "./pages/error/NotFound";
 
 function App() {
-  const { toggleTheme, selectedPageTheme, selectedBtnTheme } =
-    useContext(ThemeContext);
+  const { toggleTheme, selectedPageTheme } = useContext(ThemeContext);
+  const [currentTheme, setCurrentTheme] = useState(selectedPageTheme);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("selectedTheme");
+    if (storedTheme) {
+      setCurrentTheme(storedTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.body.style.background = currentTheme.includes("alt-page")
+      ? "linear-gradient(to bottom, #ffed8a, #fffaa9)"
+      : "linear-gradient(to bottom, #60b6d7, #a6dcef)";
+  }, [currentTheme]);
 
   const handleThemeToggle = () => {
-    toggleTheme();
-    document.body.style.background = selectedPageTheme.includes("default-page")
-      ? "linear-gradient(to bottom, #ffed8a, #fffaa9 )"
-      : "linear-gradient(to bottom, #60b6d7, #a6dcef)";
+    const newTheme =
+      currentTheme === "default-page" ? "alt-page" : "default-page";
+    setCurrentTheme(newTheme);
+    localStorage.setItem("selectedTheme", newTheme);
   };
 
   return (
-    <div className={selectedPageTheme}>
+    <div className={currentTheme}>
       <Routes>
         <Route path="/" element={<Boards />} />
         <Route path="/boards/:boardId" element={<BoardDetails />} />
-
         <Route path="/error" element={<Error />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-
-      <Button className={selectedBtnTheme} onClick={handleThemeToggle}>
+      <Button className="theme-btn" onClick={handleThemeToggle}>
         Theme
       </Button>
     </div>
